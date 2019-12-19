fcbb9b8157f347f4ff71923cf1ef2f7f0bccbcee
rlukmanov <57447988+rlukmanov@users.noreply.github.com>
2019-12-19 11:22:37 +0300

    Update [v2]

--- meshbuilder/test_solver.cpp
+++ meshbuilder/test_solver.cpp
@@ -1,44 +1,134 @@
+#include <cstring>
+#include <stdlib.h>
 #include "vmo.hpp"
 #include "toposBuild.hpp"
-#include <cstring>
+#include "Sparse.hpp"
+#include "IO.hpp"
+
+#define name_builder "prod"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-
-    if (argc < 4) 
+    if (argc != 11)
     {
         cout << "Solver v0.2 beta" << endl;
-        cout << "Usage: <format> <topology> <threads>" << endl;
+        cout << "Usage: --set <Lx> <Ly> <Nx> <Ny> <k3> <k4> <format> <topology> <threads> | \n       --ex <Lx> <Ly> <Nx> <Ny> <k3> <k4> <format> <topology> <threads>"  << endl;
         cout << "Commands:" << endl;
-        cout << "\t<format>                  Sparse matrix format. Choose between \"-csr\",\"-ellp\" and \"-coo\"." << endl; 
-        cout << "\t<topology>                Topology which sparse matrix is generated from.Choose between \"-EE\" and \"-NN\"(EE is not supported yet)." << endl; 
+        cout << "\t--ex                      An example of solving the  Ax = b using the conjugate gradient method. " << endl;
+        cout << "\t--set                     The values of non-zero values and decision vectors are set by user" << endl;
+        cout << "\t<format>                  Sparse matrix format. Choose between \"-csr\",\"-ellp\" and \"-coo\"." << endl;
+        cout << "\t<topology>                Topology which sparse matrix is generated from.Choose between \"-EE\" and \"-NN\"." << endl;
         cout << "\t<threads>                 Threads to parallel SPMV operation." << endl;
+        cout << "\t<Lx>                      Mesh X length" << endl;
+        cout << "\t<Ly>                      Mesh Y length" << endl;
+        cout << "\t<Nx>                      Number of X nodes" << endl;
+        cout << "\t<Ny>                      Number of Y nodes" << endl;
+        cout << "\t<k3>                      Number of squares in sequence" << endl;
+        cout << "\t<k4>                      Number of triangles in sequence" << endl;
         return 1;
     }
     else
     {
-        if (atoi(argv[3]) < 1)
+        FixedSizeMeshContainer<double> C;//coordinates
+        VariableSizeMeshContainer<int> topoEN;
+        VariableSizeMeshContainer<int> topoSN;
+        VariableSizeMeshContainer<int> topoNN;
+        VariableSizeMeshContainer<int> topo;
+        vector<double> x;
+        int n;
+
+        if (atoi(argv[10]) < 1)
         {
             cout << "Threads number can't be lower than 1!" << endl;
             return 1;
         }
 
-        size_t threads = atoi(argv[3]);
-        if (!strcmp("-csr",argv[1]))
+        string command = "./";
+        command += name_builder;
+        command += " --gen ";
+        for (int i = 2; i <= 7; i++) {
+            command += argv[i];
+            command += " ";
+        }
+        command += "--out";
+
+        if (system(command.c_str()) != 0) return 1;
+        cout << endl;
+
+        if (read_file(C, topoEN,topoSN)) {
+            cout << "File can't be read!" << endl;
+            return 1;
+        }
+
+        size_t threads = atoi(argv[10]);
+        if (!strcmp("-csr",argv[8]))
         {
             //csr
-            cout << "Csr" <<endl;
-            if (!strcmp("-EE",argv[2]))
+            if (!strcmp("-EE",argv[9]))
             {
                 //EE
                 cout << "EE is not currently supported yet!" <<endl;
+                return 1;
             }
-            else  if (!strcmp("-NN",argv[2]))
+            else  if (!strcmp("-NN",argv[9]))
             {
                 //NN
-                cout << "NN" <<endl;
+                topoNN = topos::build_topoNN(topoSN);
+                SparseCSR<double> matrix(topoNN);
+
+                if (!strcmp(argv[1],"--set")){
+                    double elem;
+                    vector<double> a(matrix.getValuesSize());
+                    vector<double> b;
+                    n = matrix.getDenseRows();
+
+                    M1:
+                    cout << "Enter a vector of nonzero matrix elements (" << matrix.getValuesSize() << " elements):" << endl;
+                    for (int i = 0; i < matrix.getValuesSize(); i++){
+                        cout << "\ta[" << i << "] = ";
+                        cin >> a[i];
+                    }
+                    cout << endl;
+                    matrix.setValues(a);
+                    cout << "CSR" << endl;
+                    matrix.outputMatrix();
+                    if (!vmo::Sylvester_test(matrix.getDenseMatrix(),n)){
+                        cout << "Unfortunately, the Sylvester criterion is not fulfilled. Re-enter nonzero matrix elements" << endl << endl;
+                        goto M1;
+                    }
+
+                    cout << "Enter the resulting vector:" << endl;
+                    for (int i = 0; i < n; i++){
+                        cout << "\tb[" << i << "] = ";
+                        cin >> elem;
+                        b.push_back(elem);
+                    }
+                    cout << endl;
+
+                    x = vmo::conGradSolver(matrix,b);
+                }
+                else if (!strcmp(argv[1],"--ex")){
+                    n = matrix.getDenseRows();
+                    vector<double> a(matrix.getValuesSize());
+                    vector<double> b(n);
+
+                    for (int i = 0; i < matrix.getValuesSize(); i++)
+                        a[i] = 1;
+                    a[0] = a[matrix.getValuesSize()-1] = 5;
+                    matrix.setValues(a);
+                    cout << "CSR" << endl;
+                    matrix.outputMatrix();
+                    for (int i = 0; i < n; i++)
+                        b[i] = i+1;
+
+                    x = vmo::conGradSolver(matrix,b);
+                }
+                else {
+                    cout << "Unknown first argument!" << endl;
+                    return 1;
+                }
             }
             else
             {
@@ -46,19 +136,72 @@ int main(int argc, char const *argv[])
                 return 1;
             }
         }
-        else if (!strcmp("-ellp",argv[1]))
+        else if (!strcmp("-ellp",argv[8]))
         {
             //ellp
-            cout << "Ellpack" <<endl;
-            if (!strcmp("-EE",argv[2]))
+            if (!strcmp("-EE",argv[9]))
             {
                 //EE
                 cout << "EE is not currently supported yet!" <<endl;
+                return 1;
             }
-            else  if (!strcmp("-NN",argv[2]))
+            else  if (!strcmp("-NN",argv[9]))
             {
                 //NN
-                cout << "NN" <<endl;
+                topoNN = topos::build_topoNN(topoSN);
+                SparseELL<double> matrix(topoNN);
+
+                if (!strcmp(argv[1],"--set")){
+                    double elem;
+                    vector<double> a(matrix.getValuesSize());
+                    vector<double> b;
+                    n = matrix.getDenseRows();
+
+                    M2:
+                    cout << "Enter a vector of nonzero matrix elements (" << matrix.getValuesSize() << " elements):" << endl;
+                    for (int i = 0; i < matrix.getValuesSize(); i++){
+                        cout << "\ta[" << i << "] = ";
+                        cin >> a[i];
+                    }
+                    cout << endl;
+                    matrix.setValues(a);
+                    cout << "Ellpack" <<endl;
+                    matrix.outputMatrix();
+                    if (!vmo::Sylvester_test(matrix.getDenseMatrix(),n)){
+                        cout << "Unfortunately, the Sylvester criterion is not fulfilled. Re-enter nonzero matrix elements" << endl << endl;
+                        goto M2;
+                    }
+
+                    cout << "Enter the resulting vector:" << endl;
+                    for (int i = 0; i < n; i++){
+                        cout << "\tb[" << i << "] = ";
+                        cin >> elem;
+                        b.push_back(elem);
+                    }
+                    cout << endl;
+
+                    x = vmo::conGradSolver(matrix,b);
+                }
+                else if (!strcmp(argv[1],"--ex")){
+                    n = matrix.getDenseRows();
+                    vector<double> a(matrix.getValuesSize());
+                    vector<double> b(n);
+
+                    for (int i = 0; i < matrix.getValuesSize(); i++)
+                        a[i] = 1;
+                    a[0] = a[matrix.getValuesSize()-1] = 5;
+                    matrix.setValues(a);
+                    cout << "Ellpack" <<endl;
+                    matrix.outputMatrix();
+                    for (int i = 0; i < n; i++)
+                        b[i] = i+1;
+
+                    x = vmo::conGradSolver(matrix,b);
+                }
+                else {
+                    cout << "Unknown first argument!" << endl;
+                    return 1;
+                }
             }
             else
             {
@@ -67,19 +210,72 @@ int main(int argc, char const *argv[])
             }
             
         }
-        else if (!strcmp("-coo",argv[1]))
+        else if (!strcmp("-coo",argv[8]))
         {
             //coo
-            cout << "Coord" <<endl;
-            if (!strcmp("-EE",argv[2]))
+            if (!strcmp("-EE",argv[9]))
             {
                 //EE
                 cout << "EE is not currently supported yet!" <<endl;
+                return 1;
             }
-            else  if (!strcmp("-NN",argv[2]))
+            else  if (!strcmp("-NN",argv[9]))
             {
                 //NN
-                cout << "NN" <<endl;
+                topoNN = topos::build_topoNN(topoSN);
+                SparseCOO<double> matrix(topoNN);
+
+                if (!strcmp(argv[1],"--set")){
+                    double elem;
+                    vector<double> a(matrix.getValuesSize());
+                    vector<double> b;
+                    n = matrix.getDenseRows();
+
+                    M3:
+                    cout << "Enter a vector of nonzero matrix elements (" << matrix.getValuesSize() << " elements):" << endl;
+                    for (int i = 0; i < matrix.getValuesSize(); i++){
+                        cout << "\ta[" << i << "] = ";
+                        cin >> a[i];
+                    }
+                    cout << endl;
+                    matrix.setValues(a);
+                    cout << "Coord" <<endl;
+                    matrix.outputMatrix();
+                    if (!vmo::Sylvester_test(matrix.getDenseMatrix(),n)){
+                        cout << "Unfortunately, the Sylvester criterion is not fulfilled. Re-enter nonzero matrix elements" << endl << endl;
+                        goto M3;
+                    }
+
+                    cout << "Enter the resulting vector:" << endl;
+                    for (int i = 0; i < n; i++){
+                        cout << "\tb[" << i << "] = ";
+                        cin >> elem;
+                        b.push_back(elem);
+                    }
+                    cout << endl;
+
+                    x = vmo::conGradSolver(matrix,b);
+                }
+                else if (!strcmp(argv[1],"--ex")){
+                    n = matrix.getDenseRows();
+                    vector<double> a(matrix.getValuesSize());
+                    vector<double> b(n);
+
+                    for (int i = 0; i < matrix.getValuesSize(); i++)
+                        a[i] = 1;
+                    a[0] = a[matrix.getValuesSize()-1] = 5;
+                    matrix.setValues(a);
+                    cout << "Coord" <<endl;
+                    matrix.outputMatrix();
+                    for (int i = 0; i < n; i++)
+                        b[i] = i+1;
+
+                    x = vmo::conGradSolver(matrix,b);
+                }
+                else {
+                    cout << "Unknown first argument!" << endl;
+                    return 1;
+                }
             }
             else
             {
@@ -93,5 +289,10 @@ int main(int argc, char const *argv[])
             return 1;
         }
 
+
+        cout << endl << "Solutions vector:" << endl;
+        for (int i = 0; i < n; i++)
+            cout << "\tx["<< i << "] = " << x[i] << endl;
+        cout << endl;
     }
 }
